c6729931a7871c880195241da77c9d2cb9f05171
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-09-18 22:35:15 +0300

    Added basic page for creating topics. Create logic not handled yet.

--- config/dev.exs
+++ config/dev.exs
@@ -49,8 +49,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "dev_user",
+  password: "dev_user",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
