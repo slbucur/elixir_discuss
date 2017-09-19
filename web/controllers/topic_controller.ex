c38d15918fd46f8203bbc5d4b8bfe7abfb20a6f1
Stefan-Lucian Bucur <slbucur@gmail.com>
2017-09-19 23:46:56 +0300

    Code to add topic without redirect.

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -9,7 +9,11 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-
-
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
